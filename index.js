441ef93f679cbfdfed47f0fdd698bee0a93b99b7
michaeljgaeta <michaeljgaeta@gmail.com>
2020-05-07 18:23:04 +0100

    solved lab

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase()
+    return self.connection.dropDatabase();
   })
   .then(() => {
     console.log("Added single recipe to database");
@@ -35,11 +36,19 @@ mongoose
   })
   .then(() => {
     console.log("Added many recipes to the database");
-    return Recipe.insertMany(data)
+    return Recipe.insertMany(data);
+  })
+  .then(() => {
+    console.log("Updated recipe: Rigatoni alla Genovese");
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  })
+  .then(() => {
+    console.log("Deleted recipe: Carrot Cake");
+    return Recipe.deleteOne({ title: "Carrot Cake" })
   })
   .then(() => {
     console.log("Disconnected from the database");
-    return mongoose.disconnect()
+    return mongoose.disconnect();
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);
