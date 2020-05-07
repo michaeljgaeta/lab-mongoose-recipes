a54170e622a0196e66e8d653663b944dba97aa01
michaeljgaeta <michaeljgaeta@gmail.com>
2020-05-07 18:06:13 +0100

    iteration 3 completed

--- index.js
+++ index.js
@@ -17,25 +17,29 @@ mongoose
   .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    return self.connection.dropDatabase()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log("Added single recipe to database");
     return Recipe.create({
-      title: 'Beef Stew',
-      level: 'Easy Peasy',
-      ingredients: ['beef', 'stew'],
-      cuisine: 'Basic',
-      dishType: 'main_course',
-      image: 'https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/The-Best-Beef-Stew_EXPS_TOHON19_242973_E06_11_2b.jpg',
+      title: "Beef Stew",
+      level: "Easy Peasy",
+      ingredients: ["beef", "stew"],
+      cuisine: "Basic",
+      dishType: "main_course",
+      image:
+        "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/The-Best-Beef-Stew_EXPS_TOHON19_242973_E06_11_2b.jpg",
       duration: 20,
-      creator: 'me',
-    })
-
-    return mongoose.disconnect();
+      creator: "me"
+    });
+  })
+  .then(() => {
+    console.log("Added many recipes to the database");
+    return Recipe.insertMany(data)
   })
   .then(() => {
     console.log("Disconnected from the database");
+    return mongoose.disconnect()
   })
   .catch((error) => {
     console.error("Error connecting to the database", error);
